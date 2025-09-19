7b3bd270734c20f5175b2e7e400a86a6d485b7bd
umeshtechz <umeshrym53@gmail.com>
2025-09-19 16:15:37 +0530

    Full folder update - hosting ready fixes

--- js/script.js
+++ js/script.js
@@ -1,133 +1,133 @@
-var audio=document.getElementById("song");
-var playbtn=document.getElementById("playpause");
-var prevBtn=document.getElementById("prev");
-var nextBtn=document.getElementById("next");
-var progressbar=document.getElementById("progressbar");
-var songImage=document.getElementById("song-img");
-var artistName=document.getElementById("song-artist");
-var songTitle=document.getElementById("song-title");
-var playpauseicon=document.querySelector("#playpause i");
-var thumb=document.getElementById("thumb");
-var slider=document.getElementById("range-slider");
-var startTime=document.getElementById("start-time");
-var totalTime=document.getElementById("total-time");
+var audio = document.getElementById("song");
+var playbtn = document.getElementById("playpause");
+var prevBtn = document.getElementById("prev");
+var nextBtn = document.getElementById("next");
+var progressbar = document.getElementById("progressbar");
+var songImage = document.getElementById("song-img");
+var artistName = document.getElementById("song-artist");
+var songTitle = document.getElementById("song-title");
+var playpauseicon = document.querySelector("#playpause i");
+var thumb = document.getElementById("thumb");
+var slider = document.getElementById("range-slider");
+var startTime = document.getElementById("start-time");
+var totalTime = document.getElementById("total-time");
 
+let songPlaying = false;
+let songId = 0;
 
-let songPlaying=false;
-let songId=0;
+const Images = [
+    "gulimata-song.jpg",
+    "isq-ka-raja.jpg",
+    "kalaastar-yoyo-honey-singh.jpg",
+    "kalank-by-arjit-singh.jpg",
+    "rihaayi hustle 2.0.jpg"
+];
 
-const Images=["gulimata-song.jpg",
-            "isq-ka-raja.jpg",
-            "kalaastar-yoyo-honey-singh.jpg",
-            "kalank-by-arjit-singh.jpg",
-            "Rihaayi Hustle 2.0.jpg"
-            ];
+const Songs = [
+    "Gulimata.mp3",
+    "ishq_ka_raja.mp3",
+    "Kalaastar.mp3",
+    "kalank.mp3",
+    "rihaayi_by_paradox.mp3"
+];
 
-const Songs=["Gulimata.mp3",
-            "ishq_ka_raja.mp3",
-            "Kalaastar.mp3",
-            "kalank.mp3",
-            "rihaayi_by_paradox.mp3"
-            ];
+const Artists = [
+    "Saad Lamjarred",
+    "Addy Nagar",
+    "Yo Yo Honey Singh",
+    "Arijit Singh",
+    "Paradox"
+];
 
-const Artists=["Saad Lamjarred",
-            "Addy Nagar",
-            "Yo Yo Honey Singh",
-            "Arijit Singh",
-            "Paradox"
-            ];
+const Titles = [
+    "Gulimata Arabic Song",
+    "Ishq Ka Raja",
+    "Kalaastar By Honey Singh",
+    "Kalank",
+    "Rihaayi"
+];
 
-const Titles=["Gulimata Arabic Song",
-            "Ishq Ka Raja",
-            "Kalaastar By Honey Singh",
-            "Kalank",
-            "Rihaayi"
-            ];
+// Load song data
+function loadSongData() {
+    audio.src = "./img/" + Songs[songId];
+    songImage.src = "./img/" + Images[songId];
+    songTitle.innerText = Titles[songId];
+    artistName.innerText = Artists[songId];
 
-// Function for playing the song
-playbtn.addEventListener('click',()=>{
-//    songPlaying?audio.pause():audio.play();
-if(songPlaying){
-    audio.pause();
-    songPlaying=false;
-    playpauseicon.classList.remove("fa-pause");
-    playpauseicon.classList.add("fa-play");
-    songImage.classList.remove("spin-img");
+    audio.addEventListener('loadedmetadata', () => {
+        totalTime.innerText = formatTime(audio.duration);
+    });
 }
-else{
-    audio.play();
-    songPlaying=true;
-    playpauseicon.classList.remove("fa-play");
-    playpauseicon.classList.add("fa-pause");
-    songImage.classList.add("spin-img");
-}
-})
 
-function loadSongData()
-{
-   audio.src="img/"+Songs[songId];
-   songImage.src="img/"+Images[songId];
-   songTitle.innerText=Titles[songId];
-   artistName.innerText=Artists[songId];
+// Format time in mm:ss
+function formatTime(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    if (seconds < 10) seconds = "0" + seconds;
+    if (minutes < 10) minutes = "0" + minutes;
+    return `${minutes}:${seconds}`;
 }
-loadSongData();
 
-//next Button Function
-nextBtn.addEventListener('click',()=>{
+// Play / Pause
+playbtn.addEventListener('click', () => {
+    if (songPlaying) {
+        audio.pause();
+        songPlaying = false;
+        playpauseicon.classList.remove("fa-pause");
+        playpauseicon.classList.add("fa-play");
+        songImage.classList.remove("spin-img");
+    } else {
+        audio.play();
+        songPlaying = true;
+        playpauseicon.classList.remove("fa-play");
+        playpauseicon.classList.add("fa-pause");
+        songImage.classList.add("spin-img");
+    }
+});
+
+// Next
+nextBtn.addEventListener('click', () => {
     songId++;
-    if(songId>Songs.length-1)
-    {
-        songId=0;
-    }
+    if (songId > Songs.length - 1) songId = 0;
     loadSongData();
-    switchSong()
+    if (songPlaying) audio.play();
 });
 
-//prev Button Function
-prevBtn.addEventListener('click',()=>{
+// Previous
+prevBtn.addEventListener('click', () => {
     songId--;
-    if(songId<0)
-    {
-        songId=Songs.length-1;
-    }
+    if (songId < 0) songId = Songs.length - 1;
     loadSongData();
-    switchSong();
+    if (songPlaying) audio.play();
 });
 
-//switch song
-function  switchSong(){
-    if(songPlaying==true)
-    {
-        audio.play()
-    }
-}
+// Update progress bar and time
+audio.addEventListener('timeupdate', () => {
+    if (!audio.duration) return;
+    let currentAudioTime = audio.currentTime;
+    startTime.innerText = formatTime(currentAudioTime);
 
-audio.addEventListener('timeupdate',()=>{
-    var currentAudioTime = Math.floor(audio.currentTime);
-    var currentTimePercent =(currentAudioTime/audio.duration)*100+"%";
-    thumb.style.left=currentTimePercent;
-    slider.style.width=currentTimePercent;
-})
+    let currentTimePercent = (currentAudioTime / audio.duration) * 100 + "%";
+    thumb.style.left = currentTimePercent;
+    slider.style.width = currentTimePercent;
+});
 
-
-function setTime(output,input){
-    
-}
-// Allow user to set the song's playback time using the slider
+// Seek function
 function setTime(output, input) {
-    // input me percentage milega (0-100)
-    var newTime = (input / 100) * audio.duration; // convert % to seconds
+    var newTime = (input / 100) * audio.duration;
     audio.currentTime = newTime;
     output.style.left = input + "%";
     slider.style.width = input + "%";
 }
 
-// Slider click or drag functionality
-slider.parentElement.addEventListener('click', function(e){
-    // slider ka relative click position
+// Slider click / drag
+slider.parentElement.addEventListener('click', function (e) {
     var rect = this.getBoundingClientRect();
     var offsetX = e.clientX - rect.left;
     var totalWidth = rect.width;
     var percent = (offsetX / totalWidth) * 100;
     setTime(thumb, percent);
 });
+
+// Initial load
+loadSongData();
